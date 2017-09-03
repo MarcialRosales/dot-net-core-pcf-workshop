03aa4b6d7e5b5179c2fd9fb53f641e6e8941f228
Marcial Rosales <mrosales@pivotal.io>
2017-09-03 16:09:59 +0200

    add 2 sample aplicaitons, one for dotnet-core and another for dotnet

---
+++ simple-apps/2-sample-dotnet-core/Controllers/ActuatorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace _2_sample_dotnet_core.Controllers
+{
+    [Route("mgt")]
+    public class ActuatorController : Controller
+    {
+        
+
+        // GET info
+        [HttpGet]
+        [Route("info")]
+        public AppInfo Get()
+        {
+            return new AppInfo();
+        }
+
+
+    }
+    public class AppInfo
+    {
+     
+    }
+}
---
+++ simple-apps/3-sample-dotnet/3-sample-dotnet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3_sample_dotnet.MvcApplication" Language="C#" %>
